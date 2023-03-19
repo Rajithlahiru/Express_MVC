667b660ef3c20ff0711b90e6499dd3e82e67469e
Rajith Lahiru <rajithlahiru7@gmail.com>
2023-03-19 13:44:29 +0530

    .env added and protect mongo url

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const middlewareWrapper = require('cors')
+const connectDB =require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+require('dotenv').config()
 
 // middleware
 
 app.use(express.json())
-
 app.use('/api/v1/tasks', tasks)
 
-
-
 const port = 3000
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server running on port ${port}..`))
 
-app.listen(port, console.log(`server running on port ${port}..`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
